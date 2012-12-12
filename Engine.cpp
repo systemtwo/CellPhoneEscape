7b31c9d914de02aa2d10b3dc5d0d4eb7dd2faa62
Systwo <starfruitinc@gmail.com>
2012-12-12 12:03:28 -0500

    Removed legacy DrawObj from engine

--- Engine.cpp
+++ Engine.cpp
@@ -98,55 +98,14 @@ BoundingBox GenericObj::getBounds() {
 	return bounds;
 }
 
+
 bool compareGenObjZOrder(GenericObj * i, GenericObj * j) {
 	return (i->getZOrder()) < (j->getZOrder());
 }
 
 
 
-bool compareDrawObjZOrder(DrawObj * i, DrawObj * j) {
-	return (i->zorder) < (j->zorder);
-}
 
-void Engine::addDrawObj(DrawObj *obj) {
-	objList.push_back (obj);
-	return;
-}
-
-DrawObj * Engine::makeAndAddObj(sf::Sprite * _sprite, int _zorder) {
-	//Does addDrawObj, but first wraps it for the user
-	DrawObj * tempobj = new DrawObj;
-	tempobj->sprite = _sprite;
-	tempobj->zorder = _zorder;
-	tempobj->destroy = false;
-	addDrawObj(tempobj);
-	return tempobj;
-}
-
-void Engine::drawAll() {
-	cout << "Tracking " << objList.size() << endl;
-	cleanObjList();
-	sortZOrder();
-	for (int i = 0; i < objList.size(); i++) {
-		typeid(objList[i]->sprite).name();
-		AppPointer->Draw(*(objList[i]->sprite));
-	}
-	return;
-}
-
-void Engine::sortZOrder() {
-	sort(objList.begin(), objList.end(), compareDrawObjZOrder);
-	return;
-}
-
-void Engine::cleanObjList() {
-	for (int i = 0; i < objList.size(); i++) {
-		if (objList[i]->destroy == true) {
-			delete objList[i];
-			objList.erase(objList.begin() + i);
-		}
-	}
-}
 
 void Engine::setAppPointer(sf::RenderWindow * _ap) {
 	AppPointer = _ap;
--- Engine.h
+++ Engine.h
@@ -88,15 +88,14 @@ class MemTester : public GenericObj {
 
 
 
-bool compareDrawObjZOrder(DrawObj * i, DrawObj * j);
+
 bool compareGenObjZOrder(GenericObj * i, GenericObj * j);
 
 class Engine {
 	public:
-		void addDrawObj(DrawObj *obj);
-		void drawAll();
+		
 		void setAppPointer (sf::RenderWindow * _ap);
-		DrawObj * makeAndAddObj(sf::Sprite * _sprite, int _zorder);
+		
 		
 		void addGenObj(GenericObj * genobj);
 		void drawAllGenObj();
@@ -106,8 +105,6 @@ class Engine {
 	private:
 		std::vector<DrawObj*> objList;
 		std::vector<GenericObj *> genObjList;
-		void sortZOrder();
-		void cleanObjList();
 		sf::RenderWindow * AppPointer;		
 };
 
