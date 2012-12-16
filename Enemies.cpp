3a6272ae5cc7b94cb5c3f7cf313e1f3e562cf8d2
Systwo <starfruitinc@gmail.com>
2012-12-15 22:07:43 -0500

    Begin work on SecBot collisions

--- Enemies.cpp
+++ Enemies.cpp
@@ -109,12 +109,59 @@ SecBot::SecBot(sf::RenderWindow * _ap, ResourceManager * _rm, Engine * _eng, Pla
 	//RMPointer = _rm;
 	speed = 10;
 	
+	//Set bounds
+	bounds.x = 100; //rand()%1000;
+	bounds.y = rand()%10;
+	bounds.w = sprite.GetSize().x;
+	bounds.h = sprite.GetSize().y;
+	
 	sprite.SetImage(*_rm->getImage("arrgav"));
 	setZOrder(10);
 	
 	name = "secbot";
 	
 	return;
+}
+
+void SecBot::resolveCollisions(CollisionObj co){
+	//"Ghost" enemy, allowed to go thru tiles/walls
+	if((co.collD)) { 
+		//Iterate thru objs it colides with on bottom
+		for (int i= 0; i < co.nameD.size(); i++) {
+			cout << co.nameD[i];
+			if((co.nameD[i]=="player")) {
+				cout<<"Destroy Down";
+				selfDestruct();    //Add extra effects later, in a private member function.
+			} 				
+		}
+	} 
+	
+	if ((co.collL)) {
+		for (int i= 0; i < co.nameL.size(); i++) {
+			if(co.nameL[i]=="player"){
+				cout<<"Destroy Left";
+				selfDestruct();
+			}
+		}
+	}
+	
+	if (co.collU) {
+		for(int i= 0; i < co.nameU.size(); i++) {
+			if((co.nameU[i]=="player")&&(i<co.nameU.size())) {
+				selfDestruct();
+				//cout<<"Destroy Up";
+			}
+		}
+	}
+			
+	if (co.collR) {
+		for (int i= 0; i < co.nameR.size(); i++) {
+			if((co.nameR[i]=="player")&&(i<co.nameR.size())) {
+				selfDestruct();
+				//cout<<"Destroy Right";
+			}
+		}
+	}
 }
 
 void SecBot::update(float dt) {
@@ -129,6 +176,9 @@ void SecBot::update(float dt) {
 	} else if (playptr->getBounds().y < bounds.y)  {
 		bounds.y -= speed * dt;
 	}
+	
+	//CollisionObj co = eng->detectCollisions(bounds, this);
+	//resolveCollisions(co);
 	
 	sprite.SetX(bounds.x);
 	sprite.SetY(bounds.y);
